87d7c99632e96ef9a5063dceb1f8f517c46a0293
elang8899 <elang8899@gmail.com>
2015-10-12 15:34:53 -0600

    mas tests

--- SurveyCreator/spec/views/surveys/index.html.erb_spec.rb
+++ SurveyCreator/spec/views/surveys/index.html.erb_spec.rb
@@ -1,12 +1,37 @@
 require 'rails_helper'
+require 'spec_helper'	
 
-RSpec.describe "surveys/index.html.erb", type: :view do
+describe "surveys/index.html.erb", type: :view do
+
 	before(:each) do
-    	assign(:surveys, [Survey.create!(:name => "Name"),Survey.create!(:name => "Name")])
-  	end
-	it "renders a list of surveys" do
-    	render
-    	assert_select "tr>td", :text => "Name".to_s, :count => 2
-    end
-    
+		@user = User.create!(email: "email@gmail.com", password: "password")
+		sign_in @user
+	end 
+
+	context "unpublished surveys" do 
+		before(:each) do 
+    		assign(:surveys, [Survey.create!(name: "name", status: false, user_id: @user.id)])
+		end
+
+		it "renders a list of unpublished surveys that belong to a user" do
+    		render
+    		Survey.all.each do |survey|
+    			expect(survey.user_id).to eq(@user.id)
+    			expect(rendered).to have_selector("td", text: survey.name)
+    		end 
+    	end
+	end 
+
+	context "published surveys" do 
+		before(:each) do 
+			assign(:surveys, [Survey.create!(name: "name", status: true, user_id: @user.id)])
+		end 
+
+		it "does not render surveys that have already been published" do 
+			render 
+			Survey.all.each do |survey|
+				expect(rendered).not_to have_selector('td', text: survey.name)
+			end 
+		end 
+	end 
 end
