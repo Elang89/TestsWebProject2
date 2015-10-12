d65a34c3eacf751927398cda3468c8a6b03d5e16
miumiu997 <miuyinyong@yahoo.com>
2015-10-11 21:54:54 -0600

    Cucumber Tests

--- SurveyCreator/features/manage_survey.feature
+++ SurveyCreator/features/manage_survey.feature
@@ -1,10 +1,13 @@
 Feature: Manage survey
-  In order to [goal]
-  [stakeholder]
-  wants [behaviour]
+  In order to make a survey website
+  As a user 
+  I want to create and manage surveys
   
-  Scenario: Register new survey
-    Given I am on the new survey page
-    When I fill in "name" with "name 1"
-    And I press "Create"
-    Then I should see "name 1"
+  Scenario: Create Valid Survey 
+  	Given I have no Surveys 
+  	And I am on the Surveys page 
+  	When I press "New Survey" 
+  	And I fill in "Name" with "Animals" 
+  	And I press "Create Survey" 
+  	Then I should be redirected to Survey
+  	Then I should have 1 Survey
--- SurveyCreator/features/step_definitions/survey_steps.rb
+++ SurveyCreator/features/step_definitions/survey_steps.rb
@@ -1,18 +1,28 @@
-Given /^the following surveys:$/ do |surveys|
-  Survey.create!(surveys.hashes)
+Given(/^I have no Surveys$/) do
+	Survey.delete_all
 end
 
-When /^I delete the (\d+)(?:st|nd|rd|th) survey$/ do |pos|
-  visit surveys_path
-  within("table tr:nth-child(#{pos.to_i})") do
-    click_link "Destroy"
-  end
+Given(/^I am on the Surveys page$/) do
+	visit "/surveys"
 end
 
-Then /^I should see the following surveys:$/ do |expected_surveys_table|
-
-  rows = find("table").all('tr')
-  table = rows.map { |r| r.all('th,td').map { |c| c.text.strip } }
-
-  expected_surveys_table.diff!(table) 
+When(/^I press "New Survey"$/) do 
+	click_link('New Survey')
 end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+	fill_in(arg1, :with => arg2)
+end
+ 
+When(/^I press "Create Survey"$/) do 
+	click_button('Create Survey')
+end
+
+Then(/^I should be redirected to Survey$/) do
+  request.headers['HTTP_REFERER'].should_not be_nil
+  request.headers['HTTP_REFERER'].should_not == request.request_uri
+end
+
+Then(/^I should have ([0-9]) Survey$/) do |count| 
+	expect(Survey.count).to eq(count.to_i) 
+end 
