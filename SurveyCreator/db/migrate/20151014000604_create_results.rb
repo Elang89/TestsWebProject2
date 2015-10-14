0b7575852aa4cab25aedf72cb980ac874dd69952
emmanuelrs <emmanuelrs@gmail.com>
2015-10-14 15:24:26 -0600

    arreglo bugs, falta show

--- SurveyCreator/db/migrate/20151014000604_create_results.rb
+++ SurveyCreator/db/migrate/20151014000604_create_results.rb
@@ -2,7 +2,6 @@ class CreateResults < ActiveRecord::Migration
   def change
     create_table :results do |t|
       t.references :survey, index: true, foreign_key: true
-
       t.timestamps null: false
     end
   end
